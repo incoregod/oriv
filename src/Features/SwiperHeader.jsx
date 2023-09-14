dce90e364518758f1cbe7ce94814796a1c350a02
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-14 23:32:45 +0100

    change slides order

--- src/Features/SwiperHeader.jsx
+++ src/Features/SwiperHeader.jsx
@@ -20,6 +20,23 @@ const SwiperHeader = () => {
         modules={[Pagination, Autoplay, Navigation]}
         className="mySwiper  md:!h-[760px]  "
       >
+        <SwiperSlide className="relative flex items-center sm:items-end sm:justify-start justify-center font-opensans text-white font-bold ">
+          <img
+            className="h-screen w-screen object-cover lg:object-fill "
+            src={imagem}
+            alt="foto1"
+          />
+          <div className="absolute sm:p-16  ">
+            <h1 className="text-3xl flex-wrap sm:text-4xl ">
+              {" "}
+              Jóias Únicas, Recordações Intemporais
+            </h1>
+            <p className=" text-md ">Modelos Cereja disponiveis</p>
+            <button className="mt-5   px-6 py-1 sm:px-8 sm:py-2 border-white border-2 hover:scale-110 hover:bg-black hover:border-gray-800 transition-all ease-in-out duration-300 sm:text-xl">
+              Ver mais
+            </button>
+          </div>
+        </SwiperSlide>
         <SwiperSlide className="relative flex items-center sm:items-end sm:justify-start justify-center font-opensans text-white font-bold  ">
           <video
             autoPlay
@@ -38,23 +55,6 @@ const SwiperHeader = () => {
             <p className=" text-md ">30% Desconto seleção de colares</p>
             <button className="mt-5   px-6 py-1 sm:px-8 sm:py-2 border-white border-2 hover:scale-110 hover:bg-black hover:border-gray-800 transition-all ease-in-out duration-300 sm:text-xl">
               Comprar
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="relative flex items-center sm:items-end sm:justify-start justify-center font-opensans text-white font-bold ">
-          <img
-            className="h-screen w-screen object-cover lg:object-fill "
-            src={imagem}
-            alt="foto1"
-          />
-          <div className="absolute sm:p-16  ">
-            <h1 className="text-3xl flex-wrap sm:text-4xl ">
-              {" "}
-              Jóias Únicas, Recordações Intemporais
-            </h1>
-            <p className=" text-md ">Modelos Cereja disponiveis</p>
-            <button className="mt-5   px-6 py-1 sm:px-8 sm:py-2 border-white border-2 hover:scale-110 hover:bg-black hover:border-gray-800 transition-all ease-in-out duration-300 sm:text-xl">
-              Ver mais
             </button>
           </div>
         </SwiperSlide>
