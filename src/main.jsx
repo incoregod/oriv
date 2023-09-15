1bd06ea3fa4f819ecae3891bc3c4ec48cc741d4b
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-15 14:50:00 +0100

    removed context

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { UserContextProvider } from "./Context/userContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserContextProvider>
-        <App />
-      </UserContextProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
