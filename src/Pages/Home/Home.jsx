0a40bafb4cb252fbab7b5217ce3cc70af0f991f4
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-18 13:38:13 +0100

    added some parallax effects

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Header from "./SubHome/Header";
 import LatestProducts from "./SubHome/LatestProducts";
 import Personalizar from "./SubHome/Personalizar";
 import TopProducts from "./SubHome/TopProducts";
+import { Parallax } from "react-scroll-parallax";
 
 const Home = () => {
   return (
@@ -12,19 +13,29 @@ const Home = () => {
         <Header />
       </header>
       <section className="bg-white py-10 md:px-10 px-5 2xl:py-20">
-        <About />
+        <Parallax translateY={[-10, 10]} speed={10}>
+          <About />
+        </Parallax>
       </section>
       <section className="py-10 px-5 md:px-10 lg:p-20 bg-white">
-        <Carou />
+        <Parallax translateY={[10, -20]}>
+          <Carou />
+        </Parallax>
       </section>
       <main className="py-10 lg:py-20 bg-gradient-to-b from-white via-orange-50  to-orange-50  ">
-        <Personalizar />
+        <Parallax translateY={[10, -20]}>
+          <Personalizar />
+        </Parallax>
       </main>
       <section className="py-10 px-5 md:px-10 lg:p-20 bg-gradient-to-b from-orange-50 via-orange-50  to-white">
-        <TopProducts />
+        <Parallax translateY={[0, -30]}>
+          <TopProducts />
+        </Parallax>
       </section>
       <section className="py-10 px-5 md:px-10 lg:p-20 bg-white lg:text-left text-center">
-        <LatestProducts />
+        <Parallax translateY={[-10, 0]}>
+          <LatestProducts />
+        </Parallax>
       </section>
     </div>
   );
--- src/Pages/Home/SubHome/Personalizar.jsx
+++ src/Pages/Home/SubHome/Personalizar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import foto from "../../../assets/jonathan-sanchez-Ahxw0gICnKw-unsplash.jpg";
+import { Parallax } from "react-scroll-parallax";
 
 const Personalizar = () => {
   return (
@@ -12,22 +13,24 @@ const Personalizar = () => {
         src={foto}
         alt="foto-joias"
       />
-      <div className="flex flex-col  lg:border-black lg:px-10 lg:py-20 lg:border-[1px]">
-        <h1 className="text-3xl md:text-4xl 2xl:text-6xl ">
-          Personalizar Jóias
-        </h1>
-        <p className="text-lg md:text-xl xl:text-2xl ">
-          Tem um pedido especial? Nós Ajudamos
-        </p>
-        <div className="mt-2 lg:mt-10">
-          <Link
-            className="border-black border-[1px] py-1 px-4 xl:py-2 xl:px-6 bg-black text-white  md:text-xl  xl:text-xl hover:bg-white hover:text-gray-950 ease-in-out transition-all duration-300 "
-            to={"/servicos"}
-          >
-            Os Nossos Serviços
-          </Link>
+      <Parallax translateX={[10, 0]} rotateZ={5}>
+        <div className="flex flex-col  lg:border-black lg:px-10 lg:py-20 lg:border-[1px]">
+          <h1 className="text-3xl md:text-4xl 2xl:text-6xl ">
+            Personalizar Jóias
+          </h1>
+          <p className="text-lg md:text-xl xl:text-2xl ">
+            Tem um pedido especial? Nós Ajudamos
+          </p>
+          <div className="mt-2 lg:mt-10">
+            <Link
+              className="border-black border-[1px] py-1 px-4 xl:py-2 xl:px-6 bg-black text-white  md:text-xl  xl:text-xl hover:bg-white hover:text-gray-950 ease-in-out transition-all duration-300 "
+              to={"/servicos"}
+            >
+              Os Nossos Serviços
+            </Link>
+          </div>
         </div>
-      </div>
+      </Parallax>
     </div>
   );
 };
