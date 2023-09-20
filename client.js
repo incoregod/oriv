a191e4118663cc4e0b3f5e15f4492c08c76aed06
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-21 00:15:11 +0100

    added env

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "gs46ww16",
+  projectId: import.meta.env.VITE_APP_SANITY_ID,
   dataset: "production",
   useCdn: true,
   apiVersion: "2023-09-19",
