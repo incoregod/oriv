2fc74ee4bb4b676449883d75629f1a0532ee5b6c
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-27 11:57:30 +0100

    navsmall link to home, fixed headerComponent small dev

--- src/Components/Navbar/NavSmallDev.jsx
+++ src/Components/Navbar/NavSmallDev.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Burger from "./Burger";
 import SearchCartIcons from "./SearchCartIcons";
 
@@ -5,9 +6,11 @@ const NavSmallDev = () => {
   return (
     <div className="flex items-center justify-between text-white  bg-gradientgray  bg-opacity-50 p-3 fixed top-0 left-0 right-0 z-50 ">
       <Burger />
-      <h1 className="font-cormorant text-xl font-bold tracking-wider cursor-pointer ">
-        CAROLINE BORGES
-      </h1>
+      <Link to={"/"}>
+        <h1 className="font-cormorant text-xl font-bold tracking-wider cursor-pointer ">
+          CAROLINE BORGES
+        </h1>
+      </Link>
       <SearchCartIcons />
     </div>
   );
