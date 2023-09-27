c8663c8cd13a9338389763514eb63d3ddedbd750
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-27 12:25:53 +0100

    added all filters and fixed header img on sm

--- src/Pages/Mulher/SideBarFilters.jsx
+++ src/Pages/Mulher/SideBarFilters.jsx
@@ -1,6 +1,63 @@
 import { BsPlus, BsDash } from "react-icons/bs";
 import { Link } from "react-router-dom";
+
+const filters = [
+  {
+    id: "color",
+    name: "Cor",
+    options: [
+      { value: "white", label: "Branco", checked: false },
+      { value: "blue", label: "Azul", checked: true },
+      { value: "brown", label: "Castanho", checked: false },
+      { value: "green", label: "Verde", checked: false },
+    ],
+  },
+  {
+    id: "price",
+    name: "Preço",
+    options: [
+      { value: "25", label: "€25-50", checked: false },
+      { value: "50", label: "€50-99", checked: false },
+      { value: "100", label: "€100-149", checked: false },
+      { value: "150", label: "€150-249", checked: false },
+    ],
+  },
+  {
+    id: "size",
+    name: "Tamanho",
+    options: [
+      { value: "15cm", label: "15 cm", checked: false },
+      { value: "16cm", label: "16 cm", checked: false },
+      { value: "17cm", label: "17 cm", checked: false },
+    ],
+  },
+];
+
 const SideBarFilters = ({ handleSideFilters, sideFilterOpen, categorias }) => {
+  const renderEl = filters.map((section) => {
+    return (
+      <div key={section.id}>
+        <div
+          className="flex items-center justify-between cursor-pointer py-5"
+          onClick={() => handleSideFilters(section.id)}
+        >
+          <p>{section.name}</p>
+          {sideFilterOpen[section.id] ? <BsDash /> : <BsPlus />}
+        </div>
+        {sideFilterOpen[section.id] &&
+          section.options.map((option) => (
+            <div key={option.value} className="flex gap-2 p-2">
+              <input type="checkbox" />
+              <label>
+                {section.id === "price" ? `€ ${option.value}` : option.value}
+              </label>
+            </div>
+          ))}
+        <hr />
+      </div>
+    );
+  });
+
   return (
     <div className="col-span-1 h-full hidden md:block">
       <ul className="py-5">
@@ -16,70 +73,7 @@ const SideBarFilters = ({ handleSideFilters, sideFilterOpen, categorias }) => {
         ))}
       </ul>
       <hr />
-      <div
-        className="flex items-center justify-between cursor-pointer py-5"
-        onClick={() => handleSideFilters("color")}
-      >
-        <p>Cor</p>
-        {sideFilterOpen.color ? <BsDash /> : <BsPlus />}
-      </div>
-      {sideFilterOpen.color && (
-        <div className="flex gap-2 p-2">
-          <input type="checkbox" />
-          <label>Branco</label>
-        </div>
-      )}
-      <hr />
-      <div
-        className="flex items-center justify-between cursor-pointer py-5"
-        onClick={() => handleSideFilters("price")}
-      >
-        <p>Preço</p>
-        {sideFilterOpen.price ? <BsDash /> : <BsPlus />}
-      </div>
-      {sideFilterOpen.price && (
-        <div className="flex gap-2 p-2">
-          <input type="checkbox" />
-          <label>€ 25-35</label>
-        </div>
-      )}
-      <hr />
-      <div
-        className="flex items-center justify-between cursor-pointer py-5"
-        onClick={() => handleSideFilters("size")}
-      >
-        <p>Tamanho</p>
-        {sideFilterOpen.size ? <BsDash /> : <BsPlus />}
-      </div>
-      {sideFilterOpen.size && (
-        <div className="flex flex-wrap">
-          <div className="flex gap-2 p-2">
-            <input type="checkbox" />
-            <label>17 cm</label>
-          </div>
-          <div className="flex gap-2 p-2">
-            <input type="checkbox" />
-            <label>17 cm</label>
-          </div>
-          <div className="flex gap-2 p-2">
-            <input type="checkbox" />
-            <label>17 cm</label>
-          </div>
-          <div className="flex gap-2 p-2">
-            <input type="checkbox" />
-            <label>17 cm</label>
-          </div>
-          <div className="flex gap-2 p-2">
-            <input type="checkbox" />
-            <label>17 cm</label>
-          </div>
-          <div className="flex gap-2 p-2">
-            <input type="checkbox" />
-            <label>17 cm</label>
-          </div>
-        </div>
-      )}
-      <hr />
+      {renderEl}
     </div>
   );
 };
