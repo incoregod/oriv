9c86dbb98bc119e89322b022310f6d5c2970f0c5
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-28 13:44:35 +0100

    screen

--- src/Pages/Mulher/SmSideFilter.jsx
+++ src/Pages/Mulher/SmSideFilter.jsx
@@ -2,7 +2,7 @@ import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 const SmSideFilter = ({ handleSmDevicesFilter, categorias, render }) => {
   return (
-    <div className="flex flex-col bg-white h-full w-60 fixed top-0 right-0 z-40  md:hidden overflow-y-auto py-12">
+    <div className="flex flex-col bg-white h-screen w-60 fixed top-0 right-0 z-40  md:hidden overflow-y-auto py-12">
       <div className=" border-b w-full border-opacity-50 border-gray-400 h-15 p-5 ">
         <FaTimes
           onClick={handleSmDevicesFilter}
