773c12dcddabcdc1b33d682eec775ed8f41f7605
incoregod <68203411+incoregod@users.noreply.github.com>
2023-09-29 12:28:44 +0100

    fixed

--- src/Components/SideBarComponent/SideBarComponent.jsx
+++ src/Components/SideBarComponent/SideBarComponent.jsx
@@ -3,7 +3,7 @@ const SideBarComponent = ({ children, position, type, width }) => {
   const widthSize = width ? width : "w-60";
   return (
     <div
-      className={` justify-start items-start gap-10 flex flex-col text-gray-800 bg-white h-screen ${widthSize} absolute ${position.top} ${position.side} z-[60]  ${display} overflow-y-auto `}
+      className={` justify-start items-start gap-10 flex flex-col text-gray-800 bg-white h-screen ${widthSize} fixed ${position.top} ${position.side} z-[60]  ${display} overflow-y-auto `}
     >
       {children}
     </div>
